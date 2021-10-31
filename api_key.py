e3fbe49158e01db7d6fa9daef20f5b38645a5f44
iammruni <mrunal.s15@gmail.com>
2021-10-31 19:30:41 +0530

    First Commit

---
+++ api_key.py
@@ -0,0 +1,87 @@
+import pandas as pd
+from csv import writer
+from passlib.hash import sha256_crypt
+
+
+
+key_path = './data/hashdb.csv'
+
+def store_api_key(key, permissions):
+	"""Saves the given API Key
+	"""
+	templ = ['1', '2']
+	templ[0] = str(key)
+	templ[1] = int(permissions)
+
+	with open(key_path,'a') as f_object:
+		writer_object = writer(f_object)
+		writer_object.writerow(templ)
+		f_object.close()
+	
+
+def generate(user, save=False, permissions=1):
+	"""Generate an API Key
+	"""
+	pas = sha256_crypt.encrypt(user)
+	if(save):
+		store_api_key(pas, permissions)
+	return pas
+
+
+def ver(pas, metho):
+	"""Verify the API Key
+	"""
+
+	#Permissions: 1 = GET; 2 = POST; 3 = GET&POST; 0 = BLACKLIST
+	data = pd.read_csv(key_path, header=0)
+	data = data.to_dict('list')
+	count = len(data['hash'])
+	flag = False
+	message = "This API Key does not have permission to use this method"
+	err_code = 200
+
+	for i in data['hash']:
+		if(sha256_crypt.verify(pas, i)):
+			flag =True
+			pert = data['hash'].index(str(i))
+			perm = data['permissions'][pert]
+			print(int(perm))
+			if int(perm) == 1:
+				if(metho == "get"):
+					flag = True
+					err_code = 200
+					return flag, "", err_code
+					break
+				else:
+					flag = False
+					return flag, "This API Key does not have permission to use this method", 401
+
+			elif (int(perm) == 2):
+				if(metho == "post" ):
+					flag = True
+					err_code= 200
+					return flag, "", err_code
+					break
+				else:
+					flag = False
+					return flag, "This API Key does not have permission to use this method", 401
+			elif (int(perm) == 3):
+				if(metho == "get" or metho == "post"):
+					flag = True
+					err_code= 200
+					return flag, "", err_code
+					break
+				else:
+					flag = False
+			elif (int(perm) == 0):
+				#Blacklist
+				flag = False
+				err_code= 403
+				return flag, "This API Key has been blacklisted. See you in hell, b*!", err_code
+		else:
+			flag = False
+			message = "Invalid API Key!"
+			err_code = 403
+
+
+	return flag, message, err_code
---
+++ flask_api.py
@@ -0,0 +1,73 @@
+from flask import Flask
+from flask_restful import Resource, Api, reqparse
+import ast
+import pandas as pd
+from datetime import datetime
+from flask_httpauth import HTTPBasicAuth
+import jsonify
+from passlib.hash import sha256_crypt
+import api_key
+
+
+#initialize flask app and api
+app = Flask(__name__)
+api = Api(app)
+
+
+info_path='./data/maindb.csv'
+
+
+
+
+#info endpoint
+class info(Resource):
+	def get(self):
+		parser = reqparse.RequestParser()
+		parser.add_argument('key', required=True, type=str)
+		args = parser.parse_args()
+		print(args)
+
+		if not 'key' in args:
+			return{
+			'message': {
+			'key': 'Missing required parameter. API Key missing!'
+			}
+			}, 403
+
+		else:
+			res = api_key.ver(args['key'], "get")
+			if(res[0]):
+				data = pd.read_csv(info_path, header=0)
+				data = data.to_dict('list')
+				return data, 200
+			else:
+				return{
+				'message': res[1]
+				}, res[2]
+
+
+	def post(self):
+		parser = reqparse.RequestParser()
+		parser.add_argument('day_order', required=True, type=int)
+		args = parser.parse_args()
+		if (args['day_order']>=0 and args['day_order']<=5):
+			data = pd.read_csv(info_path)
+			data['day_order'] = int(args["day_order"])
+			data['working_day'] = (True if(args["day_order"]>0 and args["day_order"]<=5) else False)
+			time_now = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+			data['lastupdated'] = time_now
+			data.to_csv(info_path, index=False)
+			return data.to_dict(), 200
+		else:
+			return {
+				'message': f"day_order arg passed ({args['day_order']}) has a range of 0-5."
+			} , 409
+
+
+
+
+#set 'info' class at 'info' endpoint
+api.add_resource(info, '/info')
+
+if __name__ == "__main__":
+	app.run(debug=True)
